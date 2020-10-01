72b6552d00f9fc308c259b4865602c99c0dd4c00
fortuneteller2k <lythe1107@gmail.com>
2020-10-02 00:11:12 +0800

    heartbeat monitoring

--- commands/utils.go
+++ commands/utils.go
@@ -1,11 +1,19 @@
 package commands
 
 import (
+	"regexp"
+
 	"github.com/andersfylling/disgord"
 )
 
 func UtilsCommand(session disgord.Session, event *disgord.MessageCreate) {
 	if event.Message.Content != "bd!utils" {
 		return
+	} else {
+		args := regexp.MustCompile("\\s+").Split(event.Message.Content, 2)
+		if args[1] == "heartbeat" {
+			heartbeatAck, _ := session.AvgHeartbeatLatency()
+			session.SendMsg(event.Ctx, event.Message.ChannelID, heartbeatAck.String())
+		}
 	}
 }
